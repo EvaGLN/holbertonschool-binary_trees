cee1d3bf074ba26e589d35cfce5baac5c2fc0683
EvaGLN <maeva.galiano@gmail.com>
2026-08-03 14:05:34 +0200

    Betty Style

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,16 +8,16 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *node;
-
-        if (!parent)
-                return (NULL);
-        node = binary_tree_node(parent, value);
-        if (!node)
-                return (NULL);
-        node->right = parent->right;
-        if (parent->right)
-                parent->right->parent = node;
-        parent->right = node;
-        return (node);
+	binary_tree_t *node;
+	
+	if (!parent)
+		return (NULL);
+	node = binary_tree_node(parent, value);
+	if (!node)
+		return (NULL);
+	node->right = parent->right;
+	if (parent->right)
+		parent->right->parent = node;
+	parent->right = node;
+	return (node);
 }
